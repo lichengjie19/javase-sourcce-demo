5521192f951987774deffc19d6ea90a6490f80b0
licjd <licjd@jiadaibao.com>
2019-12-12 21:05:02 +0800

    Integer的探索

--- src/main/java/lang/demo/IntegerDemo.java
+++ src/main/java/lang/demo/IntegerDemo.java
@@ -1,7 +1,8 @@
 package lang.demo;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.sun.javafx.image.IntPixelGetter;
+
+import java.util.*;
 
 /**
  * @author licjd
@@ -39,11 +40,47 @@ public class IntegerDemo {
 
         System.out.println(Integer.getInteger("java.vendor"));
 
-        // todo
         System.out.println(Integer.decode("0xfffffff"));
         System.out.println(0xfffffff);
+        System.out.println(011);
+
+        int x = 1;
+        x += 3.5; // x = (int)(x +3.5)
+        System.out.println(x);
+
+        // 转换为二进制
+        String binaryString = Integer.toBinaryString(123);
+        System.out.println(binaryString);
+        // 转换为十进制
+        // 二进制没有 Accepts decimal, hexadecimal, and octal numbers given by the following grammar:
+        System.out.println(Integer.decode(binaryString));
+        System.out.println(Integer.parseInt(binaryString, 2));
+        System.out.println(Integer.valueOf(binaryString, 2));
+
+        // 转换为八进制
+        String octalString = Integer.toOctalString(123);
+        System.out.println(octalString);
+        // 转换为十进制
+        System.out.println(Integer.decode("0" + octalString));
+        System.out.println(Integer.parseInt(octalString, 8));
+        System.out.println(Integer.valueOf(octalString, 8));
+
+        // 转换为十六进制
+        String hexString = Integer.toHexString(123);
+        System.out.println(hexString);
+        // 转换为十进制
+        System.out.println(Integer.decode("0x" + hexString));
+        System.out.println(Integer.parseInt(hexString, 16));
+        System.out.println(Integer.valueOf(hexString, 16));
+
+        // highest one bit
+        System.out.println(Integer.highestOneBit(1600));
+        System.out.println(Integer.lowestOneBit(1600));
+        System.out.println(Integer.numberOfLeadingZeros(1600));
+        System.out.println(Integer.numberOfTrailingZeros(1600));
 
 
+        System.out.println(Integer.reverse(1));
     }
 
 
