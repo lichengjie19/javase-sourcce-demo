b4f7d4d4c83c3e0583226f22998974f31631f278
licjd <licjd@jiadaibao.com>
2019-12-24 21:07:55 +0800

    Byte的探索

---
+++ src/main/java/lang/demo/ByteDemo.java
@@ -0,0 +1,13 @@
+package lang.demo;
+
+/**
+ * @author licjd
+ * @date 2019/12/12 21:06
+ */
+public class ByteDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Byte.BYTES);
+    }
+
+}
