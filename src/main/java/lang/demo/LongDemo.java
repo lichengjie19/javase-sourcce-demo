17a61db3dd0488e43f7441610789860e9cdae013
licjd <licjd@jiadaibao.com>
2020-01-20 14:45:02 +0800

    Long的探索

---
+++ src/main/java/lang/demo/LongDemo.java
@@ -0,0 +1,19 @@
+package lang.demo;
+
+/**
+ * @author licjd
+ * @date 2019/12/24 21:08
+ */
+public class LongDemo {
+
+    public static void main(String[] args) {
+        System.out.println(0x8000000000000000L);
+        System.out.println(0x7fffffffffffffffL);
+
+
+        System.out.println(Long.toString(1000L));
+
+        System.out.println(0x1.fffffeP+127f);
+    }
+
+}
